6b82043c5105bdaa2433043f86530b841f3c0373
Omar Rahman <orahman12@gmail.com>
2025-09-17 17:05:13 -0700

    Bundle analysis tool

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -28,7 +29,13 @@ export default defineConfig({
           }
         ]
       }
-    })
+    }),
+    ...(process.env.ANALYZE ? [visualizer({
+      filename: 'dist/stats.html',
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    })] : [])
   ],
   server: {
     port: 3000,
